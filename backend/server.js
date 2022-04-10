95d045b8cfea7b39fda557fd77ef770ac654e5d9
DarrelA <darrelaiscoding@gmail.com>
2022-04-10 22:39:03 +0800

    Connect to MongoDB and setup models: orderModel, productModel & userModel

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import 'dotenv/config';
+import colors from 'colors';
 import express from 'express';
+import mongoose from 'mongoose';
 import products from '../data/products.js';
 
 const app = express();
@@ -10,4 +12,14 @@ app.get('/api/products/:id', (req, res) =>
   res.json(products.find((product) => product._id === req.params.id))
 );
 
-app.listen(port, () => console.log(`ProShop listening on port ${port}`));
+try {
+  const connect = await mongoose.connect(process.env.MONGODB_URI);
+  console.log(`MongoDB: ${connect.connection.host}`.cyan.underline);
+} catch (error) {
+  console.error(`error: ${error.message}`.red.underline.bold);
+  process.exit(1);
+}
+
+app.listen(port, () =>
+  console.log(`ProShop listening on port ${port}`.yellow.underline)
+);
