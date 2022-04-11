bd82b85d0beaf074ef74041114673035239a3d2f
DarrelA <darrelaiscoding@gmail.com>
2022-04-11 11:31:37 +0800

    Improvement to loading UX by removing loading booleans.

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,5 +1,4 @@
 import {
-  PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
 } from '../constants/productConstant';
@@ -7,7 +6,6 @@ import axios from 'axios';
 
 const listProducts = () => async (dispatch) => {
   try {
-    dispatch({ type: PRODUCT_LIST_REQUEST });
     const { data } = await axios.get('/api/products');
     dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
   } catch (error) {
