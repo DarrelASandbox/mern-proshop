ed059dfb2f7308070da6341473d21303be2c49b9
DarrelA <darrelaiscoding@gmail.com>
2022-04-11 17:37:49 +0800

    Redux subscription for Local Storage

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -13,11 +13,7 @@ const reducer = combineReducers({
   cart: cartReducer,
 });
 
-const cartItemsFromStorage = localStorage.getItem('cartItems')
-  ? JSON.parse(localStorage.getItem('cartItems'))
-  : [];
-
-const initialState = { cart: { cartItems: cartItemsFromStorage } };
+const initialState = {};
 
 const middleware = [thunk];
 
@@ -26,5 +22,33 @@ const store = createStore(
   initialState,
   composeWithDevTools(applyMiddleware(...middleware))
 );
+
+/*
+Instead of setting LS in our action creators we can add a redux subscription,
+so that any time the cart items change in redux ie. we add an item or remove an item,
+it automatically sets LS for us.
+
+That way we don't have to imperatively set LS in every cartItem related action creator.
+
+The subscription runs when ever something is dispatched, so LS is whatever is state.cart.cartItems.
+You're only mutating LS in one place rather than multiple action creators.
+It also keeps separation of concerns with action creators only responsible for creating actions,
+without side effects, so follows rules of FP.
+*/
+
+let currentState = store.getState(); // subscription
+
+store.subscribe(() => {
+  // keep track of the previous and current state to compare changes
+  let previousState = currentState;
+  currentState = store.getState();
+  // if the cartItems change - set in localStorage
+  if (previousState.cart.cartItems !== currentState.cart.cartItems) {
+    localStorage.setItem(
+      'cartItems',
+      JSON.stringify(currentState.cart.cartItems)
+    );
+  }
+});
 
 export default store;
